26150b17727f7f330be1c2848a5231f12a792ac5
robiat <zakariyahrobiat2018@gmail.com>
2021-12-25 11:59:10 +0100

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
- let peopleEl = document.getElementById("people-el");
+let peopleEl = document.getElementById("people-el");
  let inputEl = document.getElementById("input-el");
  let input = document.getElementById("input");
  let bill = document.getElementById("bill");
+ let warning = document.getElementById("warning");
+ 
 
  let click = document.getElementById("click1");
  let click1 = document.getElementById("click2");
@@ -16,61 +18,100 @@
 
  
 click.addEventListener("click", function(){
-    let a = inputEl.value;
-     let mutiply=  a * 5/100;
-     document.getElementById("input").value =mutiply;
-   let total= (inputEl.value - mutiply) * peopleEl.value;
-document.getElementById("bill").value = total;
+    let a = bill.value;
+    let b = peopleEl.value;
+    if (peopleEl.value == 0){
+       warning.innerText = "cannot be zero";
+       peopleEl.style.border = "2px solid red";
+        }else{
+    let divide = a/b;
+     let mutiply=  divide * 5/100;
+     input.innerHTML ="$" + mutiply.toFixed(2)
+   let total= divide + mutiply
+inputEl.innerHTML = "$" + total.toFixed(2)
+    }
 
 })
 click1.addEventListener("click", function(){
-    let a = inputEl.value;
-     let mutiply=  a * 10/100;
-     document.getElementById("input").value =mutiply;
-   let total= (inputEl.value - mutiply) * peopleEl.value;
-document.getElementById("bill").value = total;
-
+  let a = bill.value;
+  let b = peopleEl.value;
+  if (peopleEl.value == 0){
+     warning.innerText = "cannot be zero"
+    peopleEl.style.border = "2px solid red";
+    }else{
+  let divide = a/b;
+   let mutiply=  divide * 10/100;
+   input.innerHTML ="$" + mutiply.toFixed(2)
+ let total= divide + mutiply
+inputEl.innerHTML = "$" + total.toFixed(2)
+    }
 })
 click2.addEventListener("click", function(){
-    let a = inputEl.value;
-     let mutiply=  a * 15/100;
-     document.getElementById("input").value =mutiply;
-   let total= (inputEl.value - mutiply) * peopleEl.value;
-document.getElementById("bill").value = total;
-
+  let a = bill.value;
+  let b = peopleEl.value;
+  if (peopleEl.value == 0){
+     warning.innerText = "cannot be zero"
+    peopleEl.style.border = "2px solid red";
+    }else{
+  let divide = a/b;
+   let mutiply=  divide * 15/100;
+   input.innerHTML ="$" + mutiply.toFixed(2)
+ let total= divide + mutiply
+inputEl.innerHTML = "$" + total.toFixed(2)
+    }
 })
 click3.addEventListener("click", function(){
-    let a = inputEl.value;
-     let mutiply=  a * 20/100;
-     document.getElementById("input").value =mutiply;
-   let total= (inputEl.value - mutiply) * peopleEl.value;
-document.getElementById("bill").value = total;
-
+  let a = bill.value;
+  let b = peopleEl.value;
+  if (peopleEl.value == 0){
+    warning.innerText = "cannot be zero"
+    peopleEl.style.border = "2px solid red";
+    }else{
+  let divide = a/b;
+   let mutiply=  divide * 20/100;
+   input.innerHTML ="$" + mutiply.toFixed(2)
+ let total= divide + mutiply
+inputEl.innerHTML = "$" + total.toFixed(2)
+    }
 })
 click4.addEventListener("click", function(){
-    let a = inputEl.value;
-     let mutiply=  a * 25/100;
-     document.getElementById("input").value =mutiply;
-   let total= (inputEl.value - mutiply) * peopleEl.value;
-document.getElementById("bill").value = total;
-
+  let a = bill.value;
+  let b = peopleEl.value;
+  if (peopleEl.value == 0){
+     warning.innerText = "cannot be zero"
+    peopleEl.style.border = "2px solid red";
+    }else{
+  let divide = a/b;
+   let mutiply=  divide * 25/100;
+   input.innerHTML ="$" + mutiply
+ let total= divide + mutiply
+inputEl.innerHTML = "$" + total.toFixed(2)
+    }
 })
 
 resetEl.addEventListener("click", function(){
-  document.getElementById("bill").value ="";
-  document.getElementById("input").value ="";
-  inputEl.value=""
+  bill.value ="";
+  input.innerHTML ="$0.00";
+  inputEl.innerHTML="$0.00"
   peopleEl.value=""
   customBtn.value=""
   
 })
 customBtn.addEventListener("click", function(){
-  let a = inputEl.value;
-  let b = customBtn.value;
-  let mutiply=  a * (b/100) ;
-  document.getElementById("input").value =mutiply;
-  let total= (a - mutiply) * peopleEl.value;
-document.getElementById("bill").value = total;
+  let a = bill.value;
+  let b = peopleEl.value;
+  if (peopleEl.value == 0){
+    warning.innerText = "cannot be zero"
+    peopleEl.style.border = "2px solid red";
+    }else{
+  let c = customBtn.value;
+  let divide = a/b;
+   let mutiply=  divide * c/100;
+   input.innerHTML ="$" + mutiply.toFixed(2)
+ let total= divide + mutiply
+inputEl.innerHTML = "$" + total.toFixed(2)
+    }
+ 
 })
 
 
